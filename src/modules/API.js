9dc6c3eaea9e2ae9bec2fb997c238691558f33db
Jonathan Dump <jbrodii77@gmail.com>
2023-06-22 15:55:48 +0300

    fix fetch

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,7 +1,10 @@
 export async function getForecast(city) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=34a8067f1d024415a9c161843231906&q=${city}&days=1&aqi=no&alerts=no`
+      `http://api.weatherapi.com/v1/forecast.json?key=34a8067f1d024415a9c161843231906&q=${city}&days=1&aqi=no&alerts=no`,
+      {
+        mode: 'cors',
+      }
     );
 
     if (!response.ok) {
