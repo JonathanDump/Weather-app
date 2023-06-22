d71610159fabb70766b4445396b7cfd7595b9f3e
Jonathan Dump <jbrodii77@gmail.com>
2023-06-22 16:08:37 +0300

    Fix api

--- src/modules/API.js
+++ src/modules/API.js
@@ -1,7 +1,7 @@
 export async function getForecast(city) {
   try {
     const response = await fetch(
-      `http://api.weatherapi.com/v1/forecast.json?key=34a8067f1d024415a9c161843231906&q=${city}&days=1&aqi=no&alerts=no`,
+      `https://api.weatherapi.com/v1/forecast.json?key=34a8067f1d024415a9c161843231906&q=${city}&days=1&aqi=no&alerts=no`,
       {
         mode: 'cors',
       }
